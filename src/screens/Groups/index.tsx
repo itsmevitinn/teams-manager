c359b8495b5e2bf7c6e8e85b84cbbf51e8dfa916
Vitor Sergio <vitorsergio111@gmail.com>
2023-11-05 22:47:00 -0300

    feat: add theme type + remove @types/styled-component-react-native

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
-import { Container, Text } from "./styles";
+import { Container } from "./styles";
 
 export function Groups() {
-  return (
-    <Container>
-      <Text>Groups</Text>
-    </Container>
-  );
+  return <Container></Container>;
 }
