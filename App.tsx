ea8788577be10c1226685628fa1c4fe58f39e9d9
Vitor Sergio <vitorsergio111@gmail.com>
2023-11-08 23:23:26 -0300

    feat: Loading component + Roboto fonts

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="light" translucent backgroundColor="transparent" />
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
